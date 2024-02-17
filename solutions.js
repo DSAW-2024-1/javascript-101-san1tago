2b271357722199da0dea6bb2f5a9c7b1fd9fcaf7
san1tago <santiagosoper@unisabana.edu.co>
2024-02-17 13:11:07 -0500

    aasd

--- solutions.js
+++ solutions.js
@@ -1,16 +1,44 @@
 // Sum of Two Numbers
 function sum(a, b) {
-  // Add your code here
+  if(typeof a !=="number" || typeof b !=="number"){
+      console.log("Los datos de entrada no son adecuados");
+  }
+  else{
+  return a + b;
+}
 }
 
 // Factorial of a Number
 function factorial(n) {
-  // Add your code here
+  if(typeof n !=="number"){
+    console.log("Los datos de entrada no son adecuados");
+}
+  else{
+    if(n === 0||n===1){
+      return 1;
+    }
+    else if(numero<0){
+      console.log("No se puede calcular factorial de negativos")
+    }
+    else{
+      let resultado = 1;
+      for(let i = 2; i<= n; i++){
+        resultado *= i;
+      }
+      return resultado;
+    }
+  }
 }
 
 // Find the Largest Number
 function findLargest(arr) {
-  // Add your code here
+  if(!arr.every(typeof arr === "number")){
+    return "Los datos de entrada no son adecuados";
+  }
+  else{
+    let largestNumber = Math.max(...arr);
+    return largestNumber;
+  }
 }
 
 // Count Vowels in a String
